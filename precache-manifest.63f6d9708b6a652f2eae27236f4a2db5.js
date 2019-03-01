dc5e3b161c1cf8fab482bbada4695015e62e63f0
Alan Pinhel de Souza <alan.pinhel@matera.com>
2019-03-01 15:33:44 -0300

    Updates

---
+++ precache-manifest.63f6d9708b6a652f2eae27236f4a2db5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "manifest.b9a383b9904b25ebe343aafb4b7a77ec.json"
+  },
+  {
+    "revision": "c8f941ff651f23aea24681ffba5aaa5a",
+    "url": "index.html"
+  },
+  {
+    "url": "icon_512x512.dfccad6bc3d072fd9b1c69fc233bb9ef.png"
+  },
+  {
+    "revision": "0500b48c3b04a21129c8c10c38f0a57a",
+    "url": "favicon.ico"
+  },
+  {
+    "url": "d728fe7b9213c1d7c24f27ba1398623c.png"
+  },
+  {
+    "url": "app.bd0693781e9b62750b45.js"
+  }
+]);
